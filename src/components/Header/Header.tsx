78d51ff2f7e188bb94f0e62c18fa9dc4b02d7e7e
cheshochek <anna_cheshok@mail.ru>
2023-03-13 09:45:06 +0300

    edit button log in

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -19,6 +19,7 @@ const Header = () => {
                 <div className={styles.userLogo}></div>
                 <div className={styles.userName}>sign in</div>
             </div>
+            <button className={styles.login}>log in</button>
         </div>
     );
 };
